b4099a2f095b1871f85e8a4b7242d72ae6ba1faf
Lei Hua <leihua2009@gmail.com>
2016-09-28 22:00:05 -0400

    added dropdown cart design

---
+++ 10-dropdown/src/vendor.ts
@@ -0,0 +1,17 @@
+// Angular 2
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+
+import 'rxjs';
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import 'jquery';
+
+
