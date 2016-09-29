d8deff4731e9c7f6d8a0e828531f68d21044305b
Lei Hua <leihua2009@gmail.com>
2016-09-28 23:44:15 -0400

    add font awesome support

---
+++ 11-font-awesome/src/vendor.ts
@@ -0,0 +1,18 @@
+// Angular 2
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+
+import 'rxjs';
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import 'jquery';
+
+
